54aa136f1d082d6b099bad924dafdbf69410f593
Techgeek2341 <103239654+Techgeek2341@users.noreply.github.com>
2022-10-06 16:21:39 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,75 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . 3 3 3 3 . . . . . . . . . . 
+        3 3 3 3 3 3 3 . . . . . . . . . 
+        3 3 3 3 3 3 3 . . . . . . . . . 
+        . . 3 3 3 3 . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, Spaceship, -100, 0)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy(effects.fire, 500)
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    info.changeLifeBy(-1)
+})
+let Bogey: Sprite = null
+let projectile: Sprite = null
+let Spaceship: Sprite = null
+info.setLife(3)
+Spaceship = sprites.create(img`
+    ....................................
+    ....................................
+    ....................................
+    ...............ccffff...............
+    ..............cddbbbf...............
+    .......ffffffcddbbbf................
+    .....ffbbbbbbbbbbbbbcfff.......ccccc
+    ...ffbbbbbbbbcbcbbbbbcccff....cdbbbc
+    ..fbbbbbbbbbbcbbcbbbbcccccfffcddbbc.
+    .fbcbbbbbbbbbbcbcbbbbccccccccbdbbf..
+    .fbbbbbbbfffbbcbbbbbccccccccccbbcf..
+    .ffbb1111fffbbcbbbbcccccccbcffbccf..
+    ..ff111111111bbbbccccccbbbcc..fbbcf.
+    ....ccccccc111bdbbbfddbccc.....ffbbf
+    ........ccccccfbdbbbfcc..........fff
+    ...............ffffff...............
+    `, SpriteKind.Player)
+controller.moveSprite(Spaceship, 100, 100)
+Spaceship.setStayInScreen(true)
+game.onUpdateInterval(500, function () {
+    Bogey = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 3 . . . . . . . . 
+        . . . . . . . 3 . . . . . . . . 
+        . . . . . . . 3 3 . . . . . . . 
+        . 3 3 . . . . . 3 3 3 3 . . . . 
+        3 3 3 3 3 3 3 3 3 3 3 3 3 . . . 
+        3 3 3 3 3 3 3 3 3 3 3 3 3 . . . 
+        . 3 3 . . . . . 3 3 3 3 . . . . 
+        . . . . . . . 3 3 . . . . . . . 
+        . . . . . . . 3 . . . . . . . . 
+        . . . . . . . 3 . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    Bogey.setVelocity(55, 0)
+    Bogey.setPosition(0, randint(5, 115))
+    Bogey.setFlag(SpriteFlag.AutoDestroy, true)
+})
